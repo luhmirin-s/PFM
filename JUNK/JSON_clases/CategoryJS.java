8c2d3dc2d4e261ad058eb5e5e28518f30a43f03d
siga111 <sergejs.luhmirins@gmail.com>
2012-08-27 15:54:52 +0300

    classes to work with JSON

---
+++ JUNK/JSON_clases/CategoryJS.java
@@ -0,0 +1,17 @@
+package main.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
+
+//An overlay type
+class CategoryJS extends JavaScriptObject {
+
+//Overlay types always have protected, zero-arg ctors
+protected CategoryJS() { }
+
+//Typically, methods on overlay types are JSNI
+public final native String getId() /*-{ return this.id; }-*/;
+public final native String getName() /*-{ return this.name; }-*/;
+public final native String getUserId() /*-{ return this.userId; }-*/;
+
+}
